1a4af851f59771125f9379abef3eb9d7f4c4fe4d
huanghr <hhr815666@dingtalk.com>
2022-01-26 17:40:56 +0800

    up

--- module/game/gomoku/board.go
+++ module/game/gomoku/board.go
@@ -1,11 +1,40 @@
 package gomoku
 
-var Board [][]int
+type Location struct {
+	X int
+	Y int
+}
 
-func init() {
-	//init board
-	Board = make([][]int, 15)
-	for i := range Board {
-		Board[i] = make([]int, 15)
+type BoardStruct struct {
+	Board [][]int
+	Steps []Location
+}
+
+var BoardIns *BoardStruct
+
+type IBoard interface {
+	Put(who, x, y int) bool
+}
+
+func NewBoard() IBoard {
+	b := make([][]int, 15)
+	for i := range b {
+		b[i] = make([]int, 15)
 	}
+	BoardIns = &BoardStruct{
+		Board: b,
+		Steps: make([]Location, 225),
+	}
+	return BoardIns
+}
+
+func (b *BoardStruct) Put(who, x, y int) bool {
+	if x < 0 || x > 14 || y < 0 || y > 14 {
+		return false
+	}
+	if b.Board[x][y] != 0 {
+		return false
+	}
+	b.Board[x][y] = who
+	return true
 }
